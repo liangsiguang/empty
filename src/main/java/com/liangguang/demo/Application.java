7202b18bb2ae96754e120d0069d5dff4373453af
梁光 <liangguang@innoval.com.cn>
2021-12-08 23:13:58 +0800

    first commit empty springboot

---
+++ src/main/java/com/liangguang/demo/Application.java
@@ -0,0 +1,16 @@
+package com.liangguang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: LiangGuang
+ * @Date: 2021/12/8 22:38
+ * @Description:
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
